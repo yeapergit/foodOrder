ddb1c945839d19947a751b96d7270265d6663cf5
pedro.ferreira <pedro.santos.ferreira@boost-it.pt>
2024-01-15 16:11:10 +0000

    fix meals url

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -11,7 +11,7 @@ const Meals = () => {
     data: loadedMeals,
     isLoading,
     error,
-  } = useHttp("http://localhost:3000/asdasdas", requestConfig, []);
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
   if (isLoading) {
     return <p className="center">Fetching meals...</p>;
